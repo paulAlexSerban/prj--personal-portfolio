70f8f6d9184d44958aec3bc27c46ccd7324694cd
paulAlexSerban <paul.alex.serban@gmail.com>
2023-01-19 09:00:44 +0200

    ci: adjust some scripts

--- scripts/package/portfolio-website.bash
+++ scripts/package/portfolio-website.bash
@@ -5,3 +5,7 @@ cd "$(dirname "$0")" || exit
 echo "📦  Package Frontend NextJS"
 mkdir -p ../../package/portfolio-website-nextjs
 cp -rfv ../../frontend/portfolio-website-nextjs/out/* ../../package/portfolio-website-nextjs
+
+echo "[info] creating .nojekyll"
+touch ../../package/portfolio-website-nextjs/.nojekyll
+echo "[info] .nojekyll created."
