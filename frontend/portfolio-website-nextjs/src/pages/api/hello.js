cc97709fbd7b44ba5875f322ab2379ad30ff3fa3
paulAlexSerban <paul.alex.serban@gmail.com>
2023-01-19 09:03:59 +0200

    ci: wip

--- frontend/portfolio-website-nextjs/src/pages/api/hello.js
+++ frontend/portfolio-website-nextjs/src/pages/api/hello.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'Hello, my name is John Doe, and I am getting crazy, I need another test, and another one, and another one, and another one, this is crazy, and some other changes, and another strage changes, i will burn' })
+  res.status(200).json({ name: 'Hello, my name is crazy, and some other changes, and another strage changes, i will burn' })
 }
