ef7ece93a4a0134ac8a7acad9dbfcf11ee335aff
paulAlexSerban <paul.alex.serban@gmail.com>
2023-06-18 17:57:27 +0300

    feat(sections): added section and extra descriptyion

--- frontend/portfolio-website/src/core/templates/Generic.template.js
+++ frontend/portfolio-website/src/core/templates/Generic.template.js
@@ -1,10 +1,9 @@
 import { Roboto } from "next/font/google";
 import { base } from "@/styles/templates/generic.module.scss";
 import dynamic from "next/dynamic";
-import { useSiteProps } from "@/context/SitePropsContext";
-import { decodeFromBase64, encodeToBase64 } from "@/core/utils/base64";
 
 const Header = dynamic(() => import("@/core/library/organisms/Header.organism"));
+const Main = dynamic(() => import("@/core/library/organisms/Main.organism"));
 
 const roboto = Roboto({
     display: "swap",
@@ -15,41 +14,11 @@ const roboto = Roboto({
 });
 
 function GenericTemplate({ children }) {
-    const { socialMediaLinks } = useSiteProps();
-    const handleMouseEnter = (el) => {
-        el.href = decodeFromBase64(el.getAttribute("href"));
-    };
-
-    const handleMouseLeave = (el) => {
-        el.href = encodeToBase64(el.getAttribute("href"));
-    };
     return (
         <div className={[base, roboto.className].join(" ")}>
             <Header />
-            {children}
-            <footer>
-                <h3>Find me on:</h3>
-                <ul>
-                    {socialMediaLinks.map((link) => {
-                        const { label, href, isEncoded } = link;
-                        return (
-                            <li key={label}>
-                                <a
-                                    href={href}
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                    onMouseEnter={isEncoded ? (e) => handleMouseEnter(e.target) : null}
-                                    onMouseLeave={isEncoded ? (e) => handleMouseLeave(e.target) : null}
-                                    onTouchStart={isEncoded ? (e) => handleMouseEnter(e.target) : null}
-                                    onTouchEnd={isEncoded ? (e) => handleMouseLeave(e.target) : null}
-                                >
-                                    {label}
-                                </a>
-                            </li>
-                        );
-                    })}
-                </ul>
-            </footer>
+            <Main>{children}</Main>
+            <footer></footer>
         </div>
     );
 }
--- frontend/portfolio-website/src/pages/index.js
+++ frontend/portfolio-website/src/pages/index.js
@@ -4,11 +4,24 @@ import Head from "next/head";
 import { useSiteProps } from "@/context/SitePropsContext";
 import { IndexPageProvider, IndexPageContext } from "@/context/pages/IndexPageContext"; // Correct the path accordingly
 import { useContext } from "react";
+import dynamic from "next/dynamic";
+import { Paragraph } from "@/core/library/atoms/typography";
+const HeroBanner = dynamic(() => import("@/core/library/organisms/HeroBanner.organism"));
+const Section = dynamic(() => import("@/core/library/organisms/Section.organism"));
+import { decodeFromBase64, encodeToBase64 } from "@/core/utils/base64";
 
 function IndexPage() {
     const pageContent = useContext(IndexPageContext);
-    const { title } = pageContent;
-    const { icons } = useSiteProps();
+    const { title, main } = pageContent;
+    const { icons, socialMediaLinks } = useSiteProps();
+
+    const handleMouseEnter = (el) => {
+        el.href = decodeFromBase64(el.getAttribute("href"));
+    };
+
+    const handleMouseLeave = (el) => {
+        el.href = encodeToBase64(el.getAttribute("href"));
+    };
 
     return (
         <>
@@ -18,12 +31,40 @@ function IndexPage() {
                 <link rel="icon" href={icons.favicon} />
             </Head>
             <GenericTemplate>
-                <main>
-                    <h2>
-                        <span>Website is under construction!</span> <br />
-                        <span>Come back soon!</span>
-                    </h2>
-                </main>
+                <HeroBanner
+                    pageTitle={main.heroBanner.pageTitle}
+                    subheading={main.heroBanner.subheading}
+                    // socialMediaLinks={siteProps.socialMediaLinks}
+                />
+                <Section
+                    headingTitle={main.section_1.title}
+                    subheadingText={"Note: Website is under construction!"}
+                    hasSeparator={false}
+                >
+                    <Paragraph>{main.section_1.content.paragraph_1}</Paragraph>
+                    <Paragraph>{main.section_1.content.paragraph_2}</Paragraph>
+                    <h3>Find me on:</h3>
+                    <ul>
+                        {socialMediaLinks.map((link) => {
+                            const { label, href, isEncoded } = link;
+                            return (
+                                <li key={label}>
+                                    <a
+                                        href={href}
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        onMouseEnter={isEncoded ? (e) => handleMouseEnter(e.target) : null}
+                                        onMouseLeave={isEncoded ? (e) => handleMouseLeave(e.target) : null}
+                                        onTouchStart={isEncoded ? (e) => handleMouseEnter(e.target) : null}
+                                        onTouchEnd={isEncoded ? (e) => handleMouseLeave(e.target) : null}
+                                    >
+                                        {label}
+                                    </a>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </Section>
             </GenericTemplate>
         </>
     );
