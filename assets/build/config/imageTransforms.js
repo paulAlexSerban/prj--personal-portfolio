15854723c2d78a9a4f51e5141764ffc218164434
paulAlexSerban <paul.alex.serban@gmail.com>
2023-06-26 10:56:55 +0300

    feat(assets): added build and placeholder image

---
+++ assets/build/config/imageTransforms.js
@@ -0,0 +1,97 @@
+// specify transforms
+const transforms = [
+  {
+    options: {
+      width: 480,
+      fit: "cover",
+      quality: 100,
+      effort: 6,
+    },
+  },
+  {
+    options: {
+      width: 960,
+      fit: "cover",
+      quality: 100,
+      effort: 6,
+    },
+  },
+  {
+    options: {
+      width: 960,
+      height: 640,
+      fit: "cover",
+      quality: 100,
+      effort: 6,
+    },
+  },
+  {
+    options: {
+      width: 1920,
+      height: 1280,
+      fit: "cover",
+      quality: 100,
+      effort: 6,
+    },
+  },
+  {
+    options: {
+      width: 1280,
+      height: 960,
+      fit: "cover",
+      quality: 100,
+      effort: 6,
+    },
+  },
+  {
+    options: {
+      width: 2560,
+      height: 1920,
+      fit: "cover",
+      quality: 100,
+      effort: 6,
+    },
+  },
+  {
+    options: {
+      width: 1600,
+      height: 900,
+      fit: "cover",
+      quality: 100,
+      effort: 6,
+
+    },
+  },
+  {
+    options: {
+      width: 3200,
+      height: 1800,
+      fit: "cover",
+      quality: 100,
+      effort: 6,
+
+    },
+  },
+  {
+    options: {
+      width: 1920,
+      height: 640,
+      fit: "cover",
+      quality: 100,
+      effort: 6,
+
+    },
+  },
+  {
+    options: {
+      width: 3840,
+      height: 1280,
+      fit: "cover",
+      quality: 100,
+      effort: 6,
+
+    },
+  },
+];
+
+export default transforms;
---
+++ assets/build/config/paths.js
@@ -0,0 +1,15 @@
+export const paths = {
+  src: {
+    assets: {
+      icons: [`source/icons/*`],
+      images: [`source/images/*`],
+      svgs: [`source/svgs/*.svg`],
+      videos: [`source/videos/*`],
+      audio: [`source/audio/*`],
+    },
+  },
+  dist: {
+    dir: `./dist`,
+    images: `dist/images`,
+  },
+};
