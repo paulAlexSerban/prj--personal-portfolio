a572a5d26b35c124d028077fb17b36ad8bb0cca6
paulAlexSerban <paul.alex.serban@gmail.com>
2023-06-26 17:34:54 +0300

    feat(tooltip): added tooltip to isonlinks

--- frontend/portfolio-website/src/core/library/atoms/LinkIcon.atom.js
+++ frontend/portfolio-website/src/core/library/atoms/LinkIcon.atom.js
@@ -1,6 +1,6 @@
 import Icon from "./Icon.atom";
 import PropTypes from "prop-types";
-import styles, { base } from "@/styles/atoms/linkIcon.module.scss";
+import styles, { base, tooltip } from "@/styles/atoms/linkIcon.module.scss";
 import { Link } from "@/core/atoms/typography";
 export default function LinkIcon({
     href = "/",
@@ -18,6 +18,7 @@ export default function LinkIcon({
             isEncoded={isEncoded}
             classNames={`${base} ${position.length > 0 ? styles[`base--${position}`] : ""}`}
         >
+            <span className={tooltip}>{label}</span>
             <Icon iconName={iconName} />
         </Link>
     );
