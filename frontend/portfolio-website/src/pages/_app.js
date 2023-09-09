cb9f8f6f172b1cc42f86380130bb982863a93013
paulAlexSerban <paul.alex.serban@gmail.com>
2023-09-09 18:41:31 +0300

    feat(gtm): added gtm

--- frontend/portfolio-website/src/pages/_app.js
+++ frontend/portfolio-website/src/pages/_app.js
@@ -48,7 +48,32 @@ function App({ Component, pageProps }) {
                         });
                     }}
                 />
+                <Script
+                id="google-tag-manager"
+                strategy="afterInteractive"
+                    onLoad={() => {
+                        (function (w, d, s, l, i) {
+                            w[l] = w[l] || [];
+                            w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+                            var f = d.getElementsByTagName(s)[0],
+                                j = d.createElement(s),
+                                dl = l != 'dataLayer' ? '&l=' + l : '';
+                            j.async = true;
+                            j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+                            f.parentNode.insertBefore(j, f);
+                        })(window, document, 'script', 'dataLayer', 'GTM-MJNXDWVR');
+                    }}
+                />
             </SitePropsProvider>
+
+            <noscript>
+                <iframe
+                    src="https://www.googletagmanager.com/ns.html?id=GTM-MJNXDWVR"
+                    height="0"
+                    width="0"
+                    style={{display: "none", visibility: "hidden"}}
+                ></iframe>
+            </noscript>
         </CookieProvider>
     );
 }
